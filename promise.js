fba1ebecf0c369cb435544c3ac681341fa985737
chayansrk64 <chayansrk64@gmail.com>
2025-09-11 22:02:07 +0600

    promise updated

--- promise.js
+++ promise.js
@@ -11,3 +11,24 @@ const getData = new Promise((resolve, reject) => {
 getData
     .then(data => console.log("resolved data", data))
     .catch(error => console.log(error))
+
+
+
+
+
+function loginUser(user, password){
+    return new Promise((resolve, reject)=> {
+        setTimeout(() => {
+            if(user === "manik" && password === "1234"){
+                resolve({user: user, password: password})
+            }else{
+                reject({error: "User and Password didn't match"})
+            }
+        }, 2000)
+    })
+}
+
+
+loginUser("manik", "1234")
+    .then(msg => console.log(msg))
+    .catch(err => console.log(err))
