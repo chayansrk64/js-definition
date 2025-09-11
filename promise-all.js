68ccf657f5d237839f4b59c712dc50a71fb19aef
chayansrk64 <chayansrk64@gmail.com>
2025-09-11 21:47:36 +0600

    promise and promise-all

--- promise-all.js
+++ promise-all.js
@@ -31,3 +31,28 @@ Promise.all([getData1, getData2, getData3])
     .catch(err => console.log(err))
 
 
+
+
+
+const fetchUsers = fetch('https://jsonplaceholder.typicode.com/users')
+const fetchPosts = fetch('https://jsonplaceholder.typicode.com/posts')
+const fetchComments = fetch('https://jsonplaceholder.typicode.com/comments')
+
+
+Promise.all([fetchUsers, fetchPosts, fetchComments])
+    .then(responses => {
+        // responses is an array of all the resolved promises
+        return Promise.all(responses.map(res => res.json()))
+    })
+    .then(data => {
+        const [users, posts, comments] = data;
+        console.log('users', users);
+        console.log('posts', posts);
+        console.log('comments', comments);
+    })
+    // If any of the promises fail, this will catch the error
+    .catch(error => console.error('Error fetching data', error))
+
+
+
+
